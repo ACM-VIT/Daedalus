67aaa184b1638f1dfb837485bd4b528186123b1b
Ubaid Usmani <usmaniubaid@gmail.com>
2019-02-07 22:10:08 +0530

    Delete test.py

--- zecide/Old_code/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import datetime
-file = pd.read_excel("zecide_data.xlsx","main_data")
-date_present = datetime.datetime.now()
-date_present = date_present.strftime("%Y-%m-%d")
-date_present = "2019-01-09"
-file = file.loc[file['date'] == date_present]
-
